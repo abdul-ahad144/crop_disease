2945feba332f06ca83f59f8adfa9f1c9f376428f
abdul-ahad144 <112452139+abdul-ahad144@users.noreply.github.com>
2026-04-04 21:07:17 +0530

    Update API key and enhance error handling
    Added error handling for weather API response.

--- utils.py
+++ utils.py
@@ -1,13 +1,19 @@
 import requests
 
-API_KEY = "7e2b0e1be93cfa497d0518e26af54841"
+API_KEY = "6426773cbc70f50ad37ef85fb05d69d3"
 
 def get_weather(city):
     url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
-    res = requests.get(url).json()
+    
+    res = requests.get(url)
+    data = res.json()
 
-    temp = res["main"]["temp"]
-    humidity = res["main"]["humidity"]
-    rainfall = res.get("rain", {}).get("1h", 0)
+    # ERROR HANDLING
+    if "main" not in data:
+        raise Exception(data.get("message", "Weather API Error"))
+
+    temp = data["main"]["temp"]
+    humidity = data["main"]["humidity"]
+    rainfall = data.get("rain", {}).get("1h", 0)
 
     return temp, humidity, rainfall
